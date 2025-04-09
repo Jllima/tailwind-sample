9cbcb518e0ff9ec482925341bdc7da634e3b67f3
Jorge <jlimajorge@gmail.com>
2025-04-09 15:38:32 -0300

    feat: tabelas

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Post.find_or_create_by!(title: "Post 1", desc: "Descrição do Post 1")
+Post.find_or_create_by!(title: "Post 2", desc: "Descrição do Post 2")
+Post.find_or_create_by!(title: "Post 3", desc: "Descrição do Post 3")
+Post.find_or_create_by!(title: "Post 4", desc: "Descrição do Post 4")
+Post.find_or_create_by!(title: "Post 5", desc: "Descrição do Post 5")
